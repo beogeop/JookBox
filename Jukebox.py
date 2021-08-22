70e6366c3a2d95fb4cd31fea9b7274f6fa2abd78
Joo Young Choi <38427759+Chejuyeong@users.noreply.github.com>
2021-08-22 17:39:26 +0900

    Update Jukebox.py

--- Jukebox.py
+++ Jukebox.py
@@ -40,7 +40,7 @@ client = commands.Bot(command_prefix = '.', intents = intents)
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=".play"))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=".p"))
     print('Logged in as {0.user}'.format(client))
 
 @client.event
