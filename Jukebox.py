bf44a106493a50526556b2a835bf09dfeb2fe943
Joo Young Choi <38427759+Chejuyeong@users.noreply.github.com>
2021-08-23 08:45:37 +0900

    Update Jukebox.py

--- Jukebox.py
+++ Jukebox.py
@@ -75,7 +75,7 @@ async def p(ctx, *, search_terms):
     embed = discord.Embed(
         title = 'Currently Playing',
         description = str(json.loads(results)['videos'][0]['title']),
-        colour = discord.Colour.dark_theme()
+        colour = discord.Colour.blue()
     )
 
     embed.set_thumbnail(url=str(json.loads(results)['videos'][0]['thumbnails'][0]))
