0235bd7bd515370c3a37db17bc9d97a927f9ab01
Chejuyeong <jooyoungchoijyc@gmail.com>
2021-09-25 16:48:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import os
+import asyncio
 from music import music
 
 load_dotenv()
@@ -12,6 +13,15 @@ bot = commands.Bot(command_prefix='.', intents = discord.Intents.all())
 async def on_ready():
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="-p"))
     print('Logged in as {0.user}'.format(bot))
+
+@bot.event
+async def on_voice_state_update(member, before, after):
+    voice_state = member.guild.voice_client
+    if voice_state is None:
+        return
+    if len(voice_state.channel.members) == 1:
+        await asyncio.sleep(1)
+        await voice_state.disconnect()
 
 bot.add_cog(music(bot))
 
