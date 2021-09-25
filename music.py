98ce4b33f8884585c4157381e2a43a308ad57613
Chejuyeong <jooyoungchoijyc@gmail.com>
2021-09-25 17:33:46 +0900

    Update music.py

--- music.py
+++ music.py
@@ -6,8 +6,24 @@ import json
 
 class music(commands.Cog):
     def __init__(self, bot):
-        self.ydl_opt = {'format': 'bestaudio', 'noplaylist': 'True'}
-        self.ffmpeg_opts = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5','options': '-vn'}
+        
+        self.ydl_opt = {
+                'format': 'bestaudio/best',
+                'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+                'restrictfilenames': True,
+                'noplaylist': True,
+                'nocheckcertificate': True,
+                'ignoreerrors': False,
+                'logtostderr': False,
+                'quiet': True,
+                'no_warnings': True,
+                'default_search': 'auto',
+                'source_address': '0.0.0.0'
+                }
+        self.ffmpeg_opts = {
+                'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+                'options': '-vn'
+                }
 
         self.bot = bot
         self.is_playing = False
