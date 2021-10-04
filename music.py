b809fecfb27a78c63a3e4ab912b4dfb091ecbc2c
Chejuyeong <jooyoungchoijyc@gmail.com>
2021-10-05 07:57:55 +0900

    Update music.py

--- music.py
+++ music.py
@@ -86,7 +86,7 @@ class music(commands.Cog):
 
         voice_channel = ctx.author.voice.channel
         
-        if ctx.author.voice.channel is None:
+        if ctx.message.author.voice is None:
             await ctx.send("You're not in the voice channel!")
         else:
             song = self.search_yt(url)
